226d45deebe1a49e7cf5e30720934e0b5eea1f15
xvza <>
2020-05-12 22:14:49 +0200

    shrd

--- bspwm/config.sh
+++ bspwm/config.sh
@@ -4,6 +4,7 @@ set -u
 ###########################################################
 source    "hexa.sh"  "share"
 ###########################################################
+bspc config normal_border_color "${hexa[border]}"
 bspc config focused_border_color "${hexa[primary]}"
 bspc config border_width 3
 ###########################################################
